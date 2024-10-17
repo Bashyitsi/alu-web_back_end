7da5c1c52447bb5f443f0b1b41aa9f66763651f5
Bashyitsi <p.shema1@alustudent.com>
2024-10-17 16:10:32 +0200

    adding redis file

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
-import requests
+'''
+Web caching module using Redis
+
+This module provides functions for caching web pages using Redis.
+It includes a decorator `count_requests` to count requests and cache
+responses for a specified duration.
+'''
+
+
 import redis
+import requests
+from typing import Callable
 from functools import wraps
 
-store = redis.Redis()
+# Initialize Redis connection
+redis_connection = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def count_requests(func: Callable) -> Callable:
+    '''
+    Decorator function to count requests and cache responses in Redis.
+
+    - func (Callable): expects a URL string as argument.
+    - Callable: caches responses and increments request count.
+    '''
+
+    @wraps(func)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        '''
+        Wrapper function to count requests and cache responses.
 
-        count_key = "count:" + url
-        html = method(url)
+        - url (str): URL of the web page to fetch and cache.
+        - str: Cached HTML content of the web page.
+        '''
+        redis_connection.incr(f"request_count:{url}")
+        cached_html = redis_connection.get(f"cached_content:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        html_content = func(url)
+        redis_connection.setex(f"cached_content:{url}", 10, html_content)
+        return html_content
+
     return wrapper
 
 
-@count_url_access
+@count_requests
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    '''
+    Function to fetch a web page using HTTP GET request.
+
+    - url (str): URL of the web page to fetch.
+    - str: HTML content of the fetched web page.
+    '''
+    response = requests.get(url)  # Perform HTTP GET request
+    return response.text  # Return HTML content of the response
