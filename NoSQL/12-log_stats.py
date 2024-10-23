34017c86379bcdf09abd42e0e5450083b215c21d
Bashyitsi <p.shema1@alustudent.com>
2024-10-23 22:02:54 +0200

    adding new features

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,16 +7,18 @@ from pymongo import MongoClient
 def print_nginx_request_logs(nginx_collection):
     '''Prints stats about Nginx request logs.
     '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
+    total_logs = nginx_collection.count_documents({})
+    print(f'{total_logs} logs')
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
+        req_count = nginx_collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {req_count}')
+    status_checks_count = nginx_collection.count_documents({
+        'method': 'GET',
+        'path': '/status'
+    })
+    print(f'{status_checks_count} status check')
 
 
 def run():
